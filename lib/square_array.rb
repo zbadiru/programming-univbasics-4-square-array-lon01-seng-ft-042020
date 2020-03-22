4b3dafca6c126ee0bc8e847c9560262d55e4d4a2
Zakari Badiru <zbadiru@live.co.uk>
2020-03-22 10:57:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,17 @@
 def square_array(array)
-counter = 0
-while array[counter] do 
-  puts {|array|[counter ** 2]}
-  count += 1
+ary = [1,2,3]
+res = []
+for n in ary do
+  res << n ** 2
+end
+res 
+end
+
+def square_array(new_array)
+new_ary = [9,10,16,25]
+new_res= []
+for n in new_ary do
+  new_res << n ** 2
+end
+new_res
 end
