2b678b8596403d67aa7e3d0c1130533065f189f8
Zakari Badiru <zbadiru@live.co.uk>
2020-03-22 10:59:55 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,9 @@
 def square_array(array)
-counter = 0
-while array[counter] do 
-  puts {|array|[counter ** 2]}
-  count += 1
+new_array = []
+  counter = 0 
+  while counter < array.length()
+  new_array.push(array[counter] * array[counter])
+  counter += 1
+  end 
+  return new_array
 end
