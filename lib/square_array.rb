005f0a0444d18c4a3d2940fc199e16e67aa082c1
Zakari Badiru <zbadiru@live.co.uk>
2020-03-22 10:59:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
 new_array = []
   counter = 0 
-  while counter < numbers.length()
-  new_array.push(numbers[counter] * numbers[counter])
+  while counter < array.length()
+  new_array.push(array[counter] * array[counter])
   counter += 1
   end 
   return new_array
